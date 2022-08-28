269de80aefe4e9a13b03c0f943b0c43ae11dddcf
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2022-08-28 22:53:21 +0300

    add empty tests

---
+++ tests/Sales.Core.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sales.Core.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/Sales.Promocode.Api.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sales.Promocode.Api.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
