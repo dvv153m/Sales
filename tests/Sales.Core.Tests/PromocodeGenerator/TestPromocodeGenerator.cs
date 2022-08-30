8b005820a9ae2f2c7c1119cd7afd09bebe2dfbc9
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2022-08-30 15:07:19 +0300

    add tests

--- tests/Sales.Core.Tests/PromocodeGenerator/TestPromocodeGenerator.cs
+++ tests/Sales.Core.Tests/PromocodeGenerator/TestPromocodeGenerator.cs
@@ -7,13 +7,28 @@ namespace Sales.Core.Tests.PromocodeGenerator
         public void Build_OnSuccess_ReturnPromocode()
         {
             //Arrange
-            int promocodeLen = 8;
+            int promocodeLen = 7;
 
             //Act
             string promocode = Helper.PromocodeGenerator.Build(promocodeLen);
 
             //Arrange
             Assert.True(promocode.Length == promocodeLen);
+        }
+
+        [Fact]
+        public void Build_ThrowArgumentException()
+        {
+            //Arrange
+            int promocodeLen = 3;
+
+            //Act            
+            //Arrange
+            Assert.Throws<ArgumentException>(
+                () => 
+                {
+                    return Helper.PromocodeGenerator.Build(promocodeLen);                    
+                });
         }
     }
 }
