ab314c36494d194062ca86356a6b071e30dec7ff
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2022-09-02 16:12:04 +0300

    add foreign key

--- src/Sales.Infrastructure.Product.Data.Dapper/Migration/AddProductTables_1.cs
+++ src/Sales.Infrastructure.Product.Data.Dapper/Migration/AddProductTables_1.cs
@@ -17,18 +17,28 @@ namespace Sales.Infrastructure.Product.Data.Dapper.Migration
                   .WithColumn("Name").AsString(50).NotNullable().Unique()
                   .WithColumn("CreatedDate").AsDateTime().NotNullable();
 
-            /*Create.Table("Product")
+            Create.Table("Product")
                   .WithColumn("Id").AsInt64().NotNullable().PrimaryKey().Identity()
                   .WithColumn("Title").AsString(150).NotNullable()
                   .WithColumn("CopyNumber").AsInt32().NotNullable()
                   .WithColumn("Price").AsInt32().NotNullable()
+                  .WithColumn("PhotoPath").AsInt32()
                   .WithColumn("CreatedDate").AsDateTime().NotNullable();            
 
             Create.Table("ProductDetail")
                   .WithColumn("Id").AsInt64().NotNullable().PrimaryKey().Identity()
                   .WithColumn("AttributeId").AsInt32().NotNullable()
-                  .WithColumn("ProductId").AsInt32().NotNullable()
-                  .WithColumn("CreatedDate").AsDateTime().NotNullable();*/
+                  .WithColumn("ProductId").AsInt64().NotNullable()
+                  .WithColumn("Value").AsString(150).NotNullable()
+                  .WithColumn("CreatedDate").AsDateTime().NotNullable();
+
+            Create.ForeignKey() 
+            .FromTable("ProductDetail").ForeignColumn("ProductId")
+            .ToTable("Product").PrimaryColumn("Id");
+
+            Create.ForeignKey()
+            .FromTable("ProductDetail").ForeignColumn("AttributeId")
+            .ToTable("Attribute").PrimaryColumn("Id");
         }
     }
 }
