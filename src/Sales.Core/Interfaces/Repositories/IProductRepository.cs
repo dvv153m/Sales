f474040da6b9602ad9ea949b1117a90d5a6164ea
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2022-09-03 20:26:31 +0300

    edit GetAll in ProductRepository

--- src/Sales.Core/Interfaces/Repositories/IProductRepository.cs
+++ src/Sales.Core/Interfaces/Repositories/IProductRepository.cs
@@ -1,15 +1,9 @@
-﻿using Sales.Contracts.Entity;
-using Sales.Contracts.Entity.Product;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sales.Contracts.Entity.Product;
 
 namespace Sales.Core.Interfaces.Repositories
 {
     public interface IProductRepository
     {
-        IEnumerable<ProductEntity> GetAll();
+        Task<IEnumerable<ProductEntity>> GetAll();
     }
 }
--- src/Sales.Infrastructure.Product.Data.Dapper/Repositories/ProductRepository.cs
+++ src/Sales.Infrastructure.Product.Data.Dapper/Repositories/ProductRepository.cs
@@ -15,7 +15,7 @@ namespace Sales.Infrastructure.Product.Data.Dapper.Repositories
             _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));            
         }
 
-        public IEnumerable<ProductEntity> GetAll()
+        public async Task<IEnumerable<ProductEntity>> GetAll()
         {
             var query = @"SELECT * FROM Product p 
                           JOIN ProductDetail d 
@@ -25,47 +25,27 @@ namespace Sales.Infrastructure.Product.Data.Dapper.Repositories
 
             using (var connection = _dbContext.CreateConnection())
             {
+                var productDict = new Dictionary<int, ProductEntity>();
+
                 var productEntities = new List<ProductEntity>();
-                var products = connection.Query<ProductEntity, ProductDetailEntity, AttributeEntity, ProductEntity>(
+                var products = await connection.QueryAsync<ProductEntity, ProductDetailEntity, AttributeEntity, ProductEntity>(
                     query, (product, productDetails, attribute) =>
                     {
+                        if (!productDict.TryGetValue(product.Id, out var currentProduct))
+                        {
+                            currentProduct = product;
+                            currentProduct.ProductDetails = new List<ProductDetailEntity>();                            
+                            productDict.Add(currentProduct.Id, currentProduct);
+                        }
+
                         productDetails.Attribute = attribute;
-                        productEntities.Add(product);
-                        return product;
+                        currentProduct.ProductDetails.Add(productDetails);  
+                        
+                        return currentProduct;
                     });
 
-                return productEntities;
+                return productDict.Values.ToList();                
             }
-        }
-
-        /*SELECT * FROM [Product].[dbo].[Product] p
-  JOIN [Product].[dbo].[ProductDetail] d 
-  ON p.Id = d.ProductId
-  JOIN [Product].[dbo].[Attribute] a
-  ON d.AttributeId = a.Id*/
-        /*public IEnumerable<ProductDetailEntity> GetAll()
-        {
-            var query = "SELECT * FROM ProductDetail p JOIN Attribute a ON p.AttributeId = a.Id";
-
-            using (var connection = _dbContext.CreateConnection())
-            {
-                var productEntities = new List<ProductDetailEntity>();
-                var products = connection.Query<ProductDetailEntity, AttributeEntity, ProductDetailEntity>(
-                    query, (productDetails, attribute) =>
-                    {
-                        productDetails.Attribute = attribute;
-                        productEntities.Add(productDetails);
-                        return productDetails;
-                    });
-
-                return productEntities;
-            }            
-        }*/
-
-        /*using (IDbConnection connection = _dbContext.CreateConnection())
-            {
-                var res = connection.Query<ProductDetailEntity>("SELECT * FROM ProductDetail");//todo async
-                return res;
-            }*/
+        }        
     }
 }
